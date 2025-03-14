ac6fd0f45e4d87bbb8e7b2b26711a32c08f97216
LeahBasson <leahbasson54@gmail.com>
2025-03-14 23:56:05 +0200

    :tada:

---
+++ escapeCharactersPy.py
@@ -0,0 +1,17 @@
+print("Newline:")
+print("I am \nLeah")
+
+print("\nBackspace:")
+print("I \bam Leah")
+
+print("\nHorizontal tab:")
+print("\tI am Leah")
+
+print("\nThe \ character:")
+print("I \\am Leah")
+
+print("\nSingle quotation mark:")
+print("It\'s Leah")
+
+print("\nDouble quotation mark:")
+print("I \"am\" Leah")
